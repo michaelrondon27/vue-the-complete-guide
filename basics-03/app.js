7cfddbc9415ab6d763ec7f31b522c635dbe954ad
michael rondon <mrondon72@gmail.com>
2020-11-25 20:49:04 +0100

    24. Exploring Event Modifiers

--- basics-03/app.js
+++ basics-03/app.js
@@ -1,6 +1,7 @@
 const app = Vue.createApp({
     data() {
         return {
+            confirmedName: '',
             counter: 0,
             name: ''
         };
@@ -9,11 +10,17 @@ const app = Vue.createApp({
         add( num ) {
             this.counter = this.counter + num;
         },
+        confirmInput() {
+            this.confirmedName = this.name;
+        },
         reduce( num ) {
             this.counter = this.counter - num;
         },
         setName( event, lastName ) {
             this.name = event.target.value + ' ' + lastName;
+        },
+        submitform() {
+            alert('Submitted!');
         }
     }
 });
