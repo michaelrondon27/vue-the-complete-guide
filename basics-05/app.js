fdf831595b9a08728fbe24bcc0896c9efe3d7311
michael rondon <mrondon72@gmail.com>
2020-11-26 14:15:04 +0100

    29. Working with Watchers

--- basics-05/app.js
+++ basics-05/app.js
@@ -1,15 +1,18 @@
 const app = Vue.createApp({
     computed: {
         fullName() {
-            if ( this.name === '' ) {
+            if ( this.name === '' || this.lastName === '' ) {
                 return '';
             }
-            return this.name + ' Rondon'; 
+
+            return this.name + ' ' + this.lastName; 
         }
     },
     data() {
         return {
             counter: 0,
+            // fullName: '',
+            lastName: '',
             name: ''
         };
     },
@@ -33,6 +36,29 @@ const app = Vue.createApp({
         setName(event, lastName) {
             this.name = event.target.value;
         }
+    },
+    watch: {
+        counter( value ) {
+            if ( value > 50 ) {
+                setTimeout( () => {
+                    this.counter = 0;
+                }, 2000);
+            }
+        },
+        // lastName( value ) {
+        //     if ( value === '' ) {
+        //         this.fullName = '';
+        //     } else {
+        //         this.fullName = this.name + ' ' + value;
+        //     }
+        // },
+        // name( value ) {
+        //     if ( value === '' ) {
+        //         this.fullName = '';
+        //     } else {
+        //         this.fullName = value + ' ' + this.lastName;
+        //     }
+        // }
     }
 });
   
