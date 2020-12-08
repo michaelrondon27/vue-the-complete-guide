fea6a8de6cc7f63f5e2ec3cc24f6223aa90f8ef9
michael rondon <mrondon72@gmail.com>
2020-12-08 19:37:09 +0100

    70. Introducing Components

--- cmp-intro-01/app.js
+++ cmp-intro-01/app.js
@@ -1,7 +1,6 @@
 const app = Vue.createApp({
     data() {
         return {
-            detailsAreVisible: false,
             friends: [
                 {
                     id: 'manuel',
@@ -17,12 +16,39 @@ const app = Vue.createApp({
                 }
             ]
         };
+    }
+});
+
+app.component('friend-contact', {
+    data() {
+        return {
+            detailsAreVisible: false,
+            friend: {
+                id: 'manuel',
+                name: 'Manuel Lorenz',
+                phone: '01234 5678 991',
+                email: 'manuel@localhost.com'
+            },
+        };
     },
     methods: {
         toggleDetails() {
             this.detailsAreVisible = !this.detailsAreVisible;
         }
-    }
+    },
+    template: `
+        <li>
+            <h2>{{ friend.name }}</h2>
+
+            <button @click="toggleDetails">{{ detailsAreVisible ? 'Hide' : 'Show' }} Details</button>
+
+            <ul v-if="detailsAreVisible">
+                <li><strong>Phone:</strong> {{ friend.phone }}</li>
+
+                <li><strong>Email:</strong> {{ friend.email }}</li>
+            </ul>
+        </li>
+    `
 });
 
 app.mount('#app');
