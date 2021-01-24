34526123371f00dc963af7f395c90e9df07c4ed3
michael rondon <mrondon72@gmail.com>
2021-01-24 21:53:40 +0100

    89. Introducing "Props" (Parent => Child Communication)

--- vue-cli-01-a-new-vue-project/src/App.vue
+++ vue-cli-01-a-new-vue-project/src/App.vue
@@ -5,8 +5,17 @@
         </header>
 
         <ul>
-            <friend-contact></friend-contact>
-            <friend-contact></friend-contact>
+            <friend-contact 
+                name="Manuel Lorenz" 
+                phone-number="0123 45678 90" 
+                email-address="manuel@localhost.com"
+            ></friend-contact>
+
+            <friend-contact
+                name="Julie Jones" 
+                phone-number="0987 654421 21" 
+                email-address="julie@localhost.com"
+            ></friend-contact>
         </ul>
     </section>
 </template>
@@ -38,7 +47,7 @@ import FriendContact from './components/FriendContact.vue'
 
 <style>
     @import url('https://fonts.googleapis.com/css2?family=Jost&display=swap');
-    
+
     * {
         box-sizing: border-box;
     }
--- vue-cli-01-a-new-vue-project/src/components/FriendContact.vue
+++ vue-cli-01-a-new-vue-project/src/components/FriendContact.vue
@@ -1,14 +1,14 @@
 <template>
     <li>
-        <h2>{{ friend.name }}</h2>
+        <h2>{{ name }}</h2>
 
         <button @click="toggleDetails()">
             {{ detailsAreVisible ? 'Hide' : 'Show' }} Details
         </button>
 
         <ul v-if="detailsAreVisible">
-            <li><strong>Phone:</strong> {{ friend.phone }}</li>
-            <li><strong>Email:</strong> {{ friend.email }}</li>
+            <li><strong>Phone:</strong> {{ phoneNumber }}</li>
+            <li><strong>Email:</strong> {{ emailAddress }}</li>
         </ul>
     </li>
 </template>
@@ -17,19 +17,18 @@
     export default {
         data() {
             return {
-                detailsAreVisible: false,
-                friend: {
-                    id: 'manuel',
-                    name: 'Manuel Lorenz',
-                    phone: '0123 45678 90',
-                    email: 'manuel@localhost.com'
-                }
+                detailsAreVisible: false
             };
         },
         methods: {
             toggleDetails() {
                 this.detailsAreVisible = !this.detailsAreVisible;
             }
-        }
+        },
+        props: [
+            'name',
+            'phoneNumber',
+            'emailAddress'
+        ]
     }
 </script>
