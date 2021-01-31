4282380e649411ebd800ac9d0d44f4ee1f53f61d
michael rondon <mrondon72@gmail.com>
2021-01-31 20:34:50 +0100

    113. Scoped Slots

--- cmp-adv-01-starting-setup/src/App.vue
+++ cmp-adv-01-starting-setup/src/App.vue
@@ -9,17 +9,26 @@
       :info-text="activeUser.description"
       :role="activeUser.role"
     ></user-info>
+
+    <course-goals>
+      <template #default="slotProps">
+        <h2>{{ slotProps.item }}</h2>
+        <p>{{ slotProps.anotherProp }}</p>
+      </template>
+    </course-goals>
   </div>
 </template>
 
 <script>
   import BadgeList from './components/BadgeList.vue';
+  import CourseGoals from './components/CourseGoals';
   import TheHeader from './components/TheHeader';
   import UserInfo from './components/UserInfo.vue';
 
   export default {
     components: {
       BadgeList,
+      CourseGoals,
       TheHeader,
       UserInfo
     },
