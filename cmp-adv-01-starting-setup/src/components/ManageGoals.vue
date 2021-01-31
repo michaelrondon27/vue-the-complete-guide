3fadcee769000ec3eab6528409f3956f6cbedc24
michael rondon <mrondon72@gmail.com>
2021-01-31 21:06:40 +0100

    117. Teleporting Elements

--- cmp-adv-01-starting-setup/src/components/ManageGoals.vue
+++ cmp-adv-01-starting-setup/src/components/ManageGoals.vue
@@ -6,13 +6,15 @@
 
         <button @click="setGoal">Set Goal</button>
 
-        <error-alert v-if="inputIsInvalid">
-            <h2>Input is invalid!</h2>
+        <teleport to="body">
+            <error-alert v-if="inputIsInvalid">
+                <h2>Input is invalid!</h2>
 
-            <p>Please enter at least a few characthers...</p>
+                <p>Please enter at least a few characthers...</p>
 
-            <button @click="confirmError">Okay</button>
-        </error-alert>
+                <button @click="confirmError">Okay</button>
+            </error-alert>
+        </teleport>
     </div>
 </template>
 
