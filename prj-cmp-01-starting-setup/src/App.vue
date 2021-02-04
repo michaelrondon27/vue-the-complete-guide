b454d4c771fcb8d6a9686cc0f59771aafc530b30
michael rondon <mrondon72@gmail.com>
2021-02-04 17:59:36 +0100

    124. Setup & First Steps

---
+++ prj-cmp-01-starting-setup/src/App.vue
@@ -0,0 +1,28 @@
+<template>
+    <ul>
+        
+    </ul>    
+</template>
+
+<script>
+    export default {
+        data() {
+            return {
+                storedResources: [
+                    { 
+                        id: 'official-guide', 
+                        title: 'Official Guide', 
+                        description: 'The Official Vue.js documentation.',
+                        link: 'https://vuejs.org'
+                    },
+                    { 
+                        id: 'google', 
+                        title: 'Google', 
+                        description: 'Learn to google...',
+                        link: 'https://google.es'
+                    }
+                ]
+            };
+        }
+    }
+</script>
